5aa0a90c16e50c695df6c3afb89a8121280f406a
jonas.mcferreira <jonas.mcferreira@gmail.com>
2016-12-09 10:49:58 +0000

    refactored canImove

---
+++ src/main/java/com/jonas/domain/Quest.java
@@ -0,0 +1,12 @@
+package com.jonas.domain;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by Jonas Ferreira on 09/12/2016.
+ */
+@ManagedBean(name = "quest", eager = true)
+@SessionScoped
+public class Quest {
+}
